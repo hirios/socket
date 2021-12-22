e8283073d33103f68204446d9223da18fd6ae445
Alan Rios <35049559+hirios@users.noreply.github.com>
2021-12-22 14:28:48 +0000

    Add files via upload

--- client.py
+++ client.py
@@ -1,18 +1,23 @@
 import socket
 
 
-HOST = "192.168.15.120" #socket.gethostbyname(socket.gethostname())
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    IP = s.getsockname()[0]
+    s.close()
+    return IP
+
+
+HOST = "201.69.127.235"
 PORT = 5000      
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    USER_ID = "alanrios" + "|" + "Google_Colab" + "|"
+
     s.connect((HOST, PORT))
-    s.send(b"COMPUTADOR")
+    s.send(str.encode(USER_ID + "Colab é toop"))
 
     while True:
         data = s.recv(1024).decode()
         print('Received', repr(data))
-
-
-
-
-
--- host.py
+++ host.py
@@ -1,28 +1,47 @@
 import socket
-import threading 
+import threading
 
 
-HOST = "192.168.15.120" #socket.gethostbyname(socket.gethostname())
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    IP = s.getsockname()[0]
+    s.close()
+    return IP
+
+
+HOST = get_ip()
 PORT = 5000
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 server.listen()
+print('--- SUBINDO SERVIDOR ---')
 
-CLIENTES = []
+CLIENTES = {}
 while True:
     conn, addr = server.accept()
-    print('Connected by', addr)
     conn.send(str.encode("CONEXÃO SUCEDIDA", "UTF-8"))
-    
-    CLIENTES.append(conn)
+    print('User IP: ', addr)
+
 
     data = conn.recv(2048)
-    if data:     
-        if data.decode() == "sinal":
-            for c in CLIENTES:
-                    try:
-                        # EXCEÇÃO CASO A CONEXAO SEJA FECHADA
-                        c.send(str.encode("PERFEITO", "UTF-8"))
-                    except:
-                        print("A CONEXÃO PROVAVELMENTE FOI FECHADA")
+    if data:
+        USER_ID, DESTINO, MSG = data.decode().split("|")
+        CLIENTES[USER_ID] = {"SESSAO": conn, "destino": DESTINO}
+
+        if DESTINO in CLIENTES and DESTINO != "":
+            CLIENTES[DESTINO]["SESSAO"].send(str.encode(MSG))
+
+
+
+
+        # if MSG == "sinal":
+        #     for c in CLIENTES:
+        #             # EXCEÇÃO CASO A CONEXAO SEJA FECHADA
+        #             try:
+        #                 c.send(str.encode("PERFEITO", "UTF-8"))
+        #             except:
+        #                 # REMOVENDO CLIENTE DA LISTA DE CLIENTES CONECTADOS
+        #                 CLIENTES.remove(c)
+        #                 print("A CONEXÃO PROVAVELMENTE FOI FECHADA")
